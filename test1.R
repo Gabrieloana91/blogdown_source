da004ea289156c4e362da61630480fa808ee9701
Gabriel Oana <gabriel.oana91@gmail.com>
2017-12-10 16:20:59 +0000

    first pass

---
+++ test1.R
@@ -0,0 +1,11 @@
+
+# 1. Blogdown - First Pass ------------------------------------------------
+
+library(blogdown)
+setwd("C:\\Users\\GabzPC\\Documents\\blogdown_source")
+
+# 2. First Build ----------------------------------------------------------
+
+new_site()
+
+
