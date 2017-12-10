218a0548b31510bcd5e5ae11dcd6a39476351f6e
Gabriel Oana <gabriel.oana91@gmail.com>
2017-12-10 17:21:05 +0000

    New Junk

--- test1.R
+++ test1.R
@@ -5,7 +5,8 @@ library(blogdown)
 setwd("C:\\Users\\GabzPC\\Documents\\blogdown_source")
 
 # 2. First Build ----------------------------------------------------------
-
+# install_theme("kakawait/hugo-tranquilpeak-theme")
 # new_site()
 build_site()
 
+serve_site()
