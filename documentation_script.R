cc7856559a4f23ca3c177c04efeaa285a186b28b
Gabriel Oana <user@Users-MacBook-Air.local>
2018-03-05 14:05:51 +0000

    Modified FonoApi

--- documentation_script.R
+++ documentation_script.R
@@ -11,10 +11,14 @@ setwd("C:\\Users\\GabzPC\\Documents\\blogdown_source")
 # 2. Post 2 - ggplot and ggplotly -----------------------------------------
 # new_post(title = "ggplot_and_ggplotly", ext = ".Rmd")
 
-# 3. Post 3 - fonoapi
-new_post(title = "FonoApi", ext = ".Rmd")
+# 3. Post 3 - fonoapi -----------------------------------------------------
+# new_post(title = "FonoApi", ext = ".Rmd")
+
+# 4. Post 4 - 
+new_post(title = "")
 
 # Final Compilation -------------------------------------------------------
+
 
 build_site()
 serve_site()
